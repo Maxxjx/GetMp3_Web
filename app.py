18f33dfe18ca697eee21814f0cd828e7184fdd75
Maxxjx <maxxssj338@example.com>
2023-04-03 19:37:27 +0530

    dg

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template,send_file
 import os
 from moviepy.editor import *
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static')
 
 @app.route('/')
 def home():
